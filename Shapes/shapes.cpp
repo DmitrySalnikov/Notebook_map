2a12239c9d4eb66456564729ada5a21d1427c85a
DmitrySalnikov <mejibkop.ru@mail.ru>
2015-05-18 01:02:49 +0300

    unique_ptr

--- Shapes/shapes.cpp
+++ Shapes/shapes.cpp
@@ -1,24 +1,21 @@
 #include "Shape.h"
-
-const size_t n = 7;
+#include <vector>
+#include <memory>
 
 int main() {
-    Shape **shapes= new Shape *[n];
+    std::vector<std::unique_ptr<Shape>> shapes;
     
-    shapes[0] = new Square(2); 
-    shapes[1] = new Circle(2); 
-    shapes[2] = new Triangle(2, 2, 2); 
-    shapes[3] = shapes[2] -> new_instance_of(3); 
-    shapes[4] = shapes[3] -> clone();
-    shapes[5] = shapes[4] -> new_instance_of_vector({2, 3, 4}); 
-    shapes[6] = shapes[5] -> new_instance_of_initializer_list({3, 4, 5}); 
+    shapes.emplace_back(std::unique_ptr<Shape>(new Square(2))); 
+    shapes.emplace_back(std::unique_ptr<Shape>(new Circle(2))); 
+    shapes.emplace_back(std::unique_ptr<Shape>(new Triangle(2, 2, 2))); 
+    shapes.emplace_back(std::unique_ptr<Shape>(shapes[2] -> new_instance_of(3))); 
+    shapes.emplace_back(std::unique_ptr<Shape>(shapes[3] -> clone()));
+    shapes.emplace_back(std::unique_ptr<Shape>(shapes[4] -> new_instance_of_vector({2, 3, 4}))); 
+    shapes.emplace_back(std::unique_ptr<Shape>(shapes[5] -> new_instance_of_initializer_list({3, 4, 5}))); 
 
+    std::size_t n = shapes.size();
     for (std::size_t i = 0; i < n; ++i)
         shapes[i] -> introduce();
     
-    for (std::size_t i = 0; i < n; ++i)
-        delete shapes[i];
-
-    delete [] shapes;
     return 0;
 }
